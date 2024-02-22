c10da61c8cdc59ff2927a124929c34e6544b5b49
Rakibul Islam <rakib.ca@gmail.com>
2024-02-22 01:55:57 -0700

    minor corrections to props

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import UserCard from "./UserCard";
 import "./user.css";
-//import data from "./user.json" assert { type: "json" };
 import user from "./user.json";
+//import data from "./user.json" assert { type: "json" };
+import UserCard from "./UserCard";
 
 function App() {
-  //const data = JSON.stringify(user);
-  //console.log(data);
-  //console.log(user);
-  //console.log(user.name);
-  //console.log(user.age);
-  //console.log(user.phoneNumber);
-  //console.log(user.address);
-  //const userArray = Object.keys(user);
-  //console.log(userArray);
-  //const data = JSON.parse(user);
-  //console.log(data[0].name);
+  // const data = JSON.stringify(user);
+  // console.log(data);
+  // console.log(user);
+  // console.log(user.name);
+  // console.log(user.age);
+  // console.log(user.phoneNumber);
+  // console.log(user.address);
 
   return (
     <>
-      <h1>hello123</h1>
+      {/* <h1>{data}</h1> */}
       <UserCard
-        name="Rakib"
-        age="45"
-        phoneNumber="256-256-2569"
-        address="123 Street NE"   />
+        name={user.name}
+        age={user.age}
+        phoneNumber={user.phoneNumber}
+        address={user.address}
+      />
     </>
   );
 }
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,13 +1,14 @@
-
-
-const UserCard = (name, age, phoneNumber, address) => {
+const UserCard = ({ name, age, phoneNumber, address }) => {
   return (
     <div className="card">
-      <h2 className="name">Name:{name}</h2>
+      <h2 className="name">Name: {name}</h2>
       <div className="body">
-        <div className="label">Age:{age}</div>
-        <div className="label">Phone Number:{phoneNumber}</div>
-        <div className="label">Address:{address}</div>
+        <div className="label">Age:</div>
+        <div>{age}</div>
+        <div className="label">Phone Number:</div>
+        <div>{phoneNumber}</div>
+        <div className="label">Address:</div>
+        <div>{address}</div>
       </div>
     </div>
   );
